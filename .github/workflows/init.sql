215039414574575d9c7b7414c9ecd60f1b58e1ae
hogehoge-satosato <75717574+hogehoge-satosato@users.noreply.github.com>
2025-10-21 14:48:21 +0900

    Update init.sql

--- .github/workflows/init.sql
+++ .github/workflows/init.sql
@@ -1,7 +1,4 @@
 -- init.sql
-CREATE ROLE root WITH LOGIN PASSWORD 'postgres';
-CREATE DATABASE root OWNER root;
-\c root
 CREATE TABLE user_info (
   login_id VARCHAR PRIMARY KEY,
   password VARCHAR NOT NULL,
